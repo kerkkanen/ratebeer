1e223594e85d454989298d48414490053981806e
kerkkanen <katja.kerkkanen@helsinki.fi>
2022-10-24 15:35:12 +0300

    Fixing asset folder

--- app/assets/javascript/application.js
+++ app/assets/javascript/application.js
@@ -1,10 +1,16 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
 
+
 import "@hotwired/turbo-rails"
 import "controllers"
 
-import { beers } from "custom/utils";
+import { beers } from "./custom/utils";
 
 beers();
+
+import { breweries } from "./custom/brew";
+
+breweries();
